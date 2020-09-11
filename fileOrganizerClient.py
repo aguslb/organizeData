87e79363dc8570378f5ce6a64c2823d99ebe3bff
Agustin Velasco Torres <aguslb@icloud.com>
2020-09-11 00:56:54 -0500

    changes to compress

--- fileOrganizerClient.py
+++ fileOrganizerClient.py
@@ -1,11 +1,12 @@
 import os
-import py7zr
+import tarfile
 import pysftp
 import hashlib
 from pathlib import Path
 
+
 class FileOrganizerClient:
-    
+
     usrName = "zid_0"
     MD5File = "MD5.check"
     host = "hpz220w.local"
@@ -13,27 +14,28 @@ class FileOrganizerClient:
     succesFile = "MD5Result.check"
     privateKey = "/Users/agus/.ssh/id_rsa.fileOrg"
 
-    #envia los archivos con SFTP remotamente
+    # envia los archivos con SFTP remotamente
     def sendFiles(self, filesParam):
         homepath = self.getHomeBridgePath()
         try:
             with pysftp.Connection(self.host, username=self.usrName, private_key=self.privateKey) as sftp:
                 with sftp.cd(self.remoteHomeWin):
-                   for activeFile in filesParam:
-                       fileTosend = homepath + os.path.sep + activeFile
-                       sftp.put(fileTosend)
+                    for activeFile in filesParam:
+                        fileTosend = homepath + os.path.sep + activeFile
+                        sftp.put(fileTosend, callback=lambda x, y: print(
+                            "faltan {} de {}".format(((y-x)/1024),y)))
         finally:
             sftp.close()
 
-    #Obtiene el home path de Bridge
+    # Obtiene el home path de Bridge
     def getHomeBridgePath(self):
         return str(Path.home()) + os.path.sep + "Bridge"
-    
-    #Genera un MD5 de un archivo
-    def createMD5Checksum(self, fileParam):
-        return hashlib.md5(open(fileParam,'rb').read()).hexdigest()
 
-    #obtiene una lista de Archivos y directorios
+    # Genera un MD5 de un archivo
+    def createMD5Checksum(self, fileParam):
+        return hashlib.md5(open(fileParam, 'rb').read()).hexdigest()
+
+    # obtiene una lista de Archivos y directorios
     def getListFilesAndDir(self, path):
         files = []
         directory = []
@@ -44,43 +46,50 @@ class FileOrganizerClient:
             dirP.extend(dirpath)
             break
         return files, directory, dirP
-    
-    #Check file on client
+
+    # Check file on client
     def md5CheckFile(self, listFilesParam, extend):
         home = self.getHomeBridgePath()
-        homeMD5File = str(home) + str(os.path.sep) + str(self.MD5File) + str(extend)
+        homeMD5File = str(home) + str(os.path.sep) + \
+            str(self.MD5File) + str(extend)
         if os.path.exists(homeMD5File):
             os.remove(homeMD5File)
         try:
             with open(str(homeMD5File), "a") as fp:
                 for i in listFilesParam:
                     activeFile = str(home) + str(os.path.sep) + i
-                    fp.write(str(i) + "," + str(self.createMD5Checksum(activeFile))+"\n")
+                    fp.write(str(i) + "," +
+                             str(self.createMD5Checksum(activeFile))+"\n")
                 fp.close()
         finally:
             fp.close()
 
-    #Entry point
+    # Entry point
     def clientExecute(self):
         homepath = self.getHomeBridgePath()
         resultFilePath = homepath + os.path.sep + self.succesFile
         if os.path.exists(resultFilePath):
             try:
-             with open(resultFilePath) as fp:
-                 for cnt, line in enumerate(fp):
-                     print("Line {}: {}".format(cnt, line))
-                     rmLineFilePath = homepath + os.path.sep + line
-                     if os.path.exists(rmLineFilePath):
-                        os.remove(rmLineFilePath)
+                with open(resultFilePath) as fp:
+                    for cnt, line in enumerate(fp):
+                        print("Line {}: {}".format(cnt, line))
+                        rmLineFilePath = homepath + os.path.sep + line
+                        if os.path.exists(rmLineFilePath):
+                            os.remove(rmLineFilePath)
             finally:
                 fp.close()
                 os.remove(resultFilePath)
         listFileDir = self.getListFilesAndDir(homepath)
         for activeDir in listFileDir[1]:
-            path7zip = homepath + os.path.sep + activeDir
-            with py7zr.SevenZipFile(str(path7zip + '.7z'), 'w') as archive:
-                archive.writeall(path7zip, 'base')
-            listFileDir[0].append(str(activeDir + '.7z'))
-        self.md5CheckFile(listFileDir[0],"")
+            pathTGZ = homepath + os.path.sep + activeDir
+            with tarfile.open(pathTGZ + ".tgz", "w:gz") as tar:
+                for name in os.listdir(pathTGZ):
+                    tar.add(pathTGZ + os.path.sep + name)
+            listFileDir[0].append(str(activeDir + ".tgz"))
+        self.md5CheckFile(listFileDir[0], "")
         listFileDir[0].append(self.MD5File)
         self.sendFiles(listFileDir[0])
+
+
+fOC = FileOrganizerClient()
+fOC.clientExecute()
--- fileOrganizerServer.py
+++ fileOrganizerServer.py
@@ -10,6 +10,7 @@ from pathlib import Path
 from ExifTool import ExifTool
 from geopy.geocoders import Nominatim
 
+
 class FileOrganizatorServer:
 
     errorTimesToTry = 2
@@ -30,23 +31,23 @@ class FileOrganizatorServer:
     def __init__(self, errorTimesToTry):
         self.errorTimesToTry = errorTimesToTry
 
-    #Obtiene el home path de Bridge
+    # Obtiene el home path de Bridge
     def getHomeBridgePath(self):
         return str(Path.home()) + os.path.sep + "Bridge"
 
-    #Load file who has all info from GPS
+    # Load file who has all info from GPS
     def loadGeoGPSDic(self):
         with open(self.geoGPSFile) as f:
             for line in f:
                 (key, val) = line.split(":")
                 self.geoDic[key] = val
         f.close
-    
-    #Genera un MD5 de un archivo
-    def createMD5Checksum(self, fileParam):
-        return hashlib.md5(open(fileParam,'rb').read()).hexdigest()
 
-    #Send files
+    # Genera un MD5 de un archivo
+    def createMD5Checksum(self, fileParam):
+        return hashlib.md5(open(fileParam, 'rb').read()).hexdigest()
+
+    # Send files
     def sendAFile(self, pathParam):
         try:
             with pysftp.Connection(self.usrNameMac, username=self.usrNameMac, private_key=self.privateKeyToMac) as sftp:
@@ -54,23 +55,25 @@ class FileOrganizatorServer:
                     sftp.put(pathParam)
         finally:
             sftp.close()
-    
-    #Check the file that has all MD5 codes
+
+    # Check the file that has all MD5 codes
     def md5CheckFile(self, listFilesParam, extend):
         home = self.getHomeBridgePath()
-        homeMD5File = str(home) + str(os.path.sep) + str(self.MD5File) + str(extend)
+        homeMD5File = str(home) + str(os.path.sep) + \
+            str(self.MD5File) + str(extend)
         if os.path.exists(homeMD5File):
             os.remove(homeMD5File)
         try:
             with open(str(homeMD5File), "a") as fp:
                 for i in listFilesParam[0]:
                     activeFile = str(home) + str(os.path.sep) + i
-                    fp.write(str(i) + "," + str(self.createMD5Checksum(activeFile))+"\n")
+                    fp.write(str(i) + "," +
+                             str(self.createMD5Checksum(activeFile))+"\n")
                 fp.close()
         finally:
             fp.close()
 
-    #Obtiene una lista de Archivos y directorios
+    # Obtiene una lista de Archivos y directorios
     def getListFilesAndDir(self, path):
         files = []
         directory = []
@@ -81,28 +84,30 @@ class FileOrganizatorServer:
             dirP.extend(dirpath)
             break
         return files, directory, dirP
-    
-    #Modify date time to used in a Path
+
+    # Modify date time to used in a Path
     def modifyDateTime(self, dateTime, addTime):
-        dateTimeFormated = dateTime.replace(':','_')
+        dateTimeFormated = dateTime.replace(':', '_')
         if addTime:
-            dateTimeFormated = dateTimeFormated.replace(' ','_')
+            dateTimeFormated = dateTimeFormated.replace(' ', '_')
         else:
             dateTimeFormated = dateTimeFormated.partition(" ")[0]
         return dateTimeFormated
-    
-    #Compare MD5 file 
+
+    # Compare MD5 file
     def compareMD5(self):
         try:
-            filePath = str(self.getHomeBridgePath()) + os.path.sep + str(self.MD5File)
+            filePath = str(self.getHomeBridgePath()) + \
+                os.path.sep + str(self.MD5File)
             filePathCopied = str(filePath)+str(self.LOCAL_STR)
-            succesFile = str(self.getHomeBridgePath()) + os.path.sep + str(self.succesFile)
+            succesFile = str(self.getHomeBridgePath()) + \
+                os.path.sep + str(self.succesFile)
             if os.path.exists(succesFile):
                 os.remove(succesFile)
             with open(str(filePath)) as f:
                 myList = list(f)
                 with open(filePathCopied) as g:
-                    s = mmap.mmap(g.fileno(), 0, access=mmap.ACCESS_READ) 
+                    s = mmap.mmap(g.fileno(), 0, access=mmap.ACCESS_READ)
                     with open(str(succesFile), "a") as fp:
                         for elementActive in myList:
                             elementActive = elementActive.rstrip("\n")
@@ -114,7 +119,7 @@ class FileOrganizatorServer:
             fp.close()
             s.close()
 
-    #Generate path and pattern to copy   
+    # Generate path and pattern to copy
     def generatePathPatternToCopy(self, jsonResult):
         dataDict = dict(jsonResult[0])
         fullLocationDict = ""
@@ -123,15 +128,17 @@ class FileOrganizatorServer:
         if not 'ExifTool:Error' in dataDict:
             FileType = dataDict['File:FileType']
             if 'Composite:GPSPosition' in dataDict:
-                fullLocationDict = self.getGPSLocationPath(dataDict['Composite:GPSPosition'])
+                fullLocationDict = self.getGPSLocationPath(
+                    dataDict['Composite:GPSPosition'])
             if 'EXIF:DateTimeOriginal' in dataDict:
                 dateTimeFormated = dataDict['EXIF:DateTimeOriginal']
             else:
                 dateTimeFormated = dataDict['File:FileModifyDate']
             dateTimeFormated = self.modifyDateTime(dateTimeFormated, False)
             if 'File:ImageWidth' in dataDict and 'File:ImageHeight' in dataDict:
-                imageSize = str(dataDict['File:ImageWidth']) + "_X_" + str(dataDict['File:ImageHeight']) 
-            pathPattern = FileType + os.path.sep + dateTimeFormated 
+                imageSize = str(dataDict['File:ImageWidth']) + \
+                    "_X_" + str(dataDict['File:ImageHeight'])
+            pathPattern = FileType + os.path.sep + dateTimeFormated
             if 'Composite:GPSPosition' in dataDict:
                 pathPattern = pathPattern + os.path.sep + fullLocationDict
             if 'File:ImageWidth' in dataDict and 'File:ImageHeight' in dataDict:
@@ -140,11 +147,12 @@ class FileOrganizatorServer:
             fileName = str(dataDict['File:FileName'])
             index = int(fileName.rfind(".")) + 1
             fileType = fileName[index:]
-            dateTimeFormated = self.modifyDateTime(dataDict['File:FileModifyDate'], False)
+            dateTimeFormated = self.modifyDateTime(
+                dataDict['File:FileModifyDate'], False)
             pathPattern = fileType + os.path.sep + dateTimeFormated
         return pathPattern
-    
-    #Check if the dir exist
+
+    # Check if the dir exist
     def checkDir(self, newPathParam, homePathParam):
         if not bool(newPathParam):
             return False
@@ -153,35 +161,41 @@ class FileOrganizatorServer:
             os.makedirs(newPath, exist_ok=True)
         return os.path.isdir(newPath)
 
-    #File Organizator
+    # File Organizator
     def fileOrganizator(self, filesParam, pathFileOrgParam, goToDir):
         for fileActive in filesParam:
             fileMetadataActive = pathFileOrgParam + os.path.sep + fileActive
-            pathToCopy = self.generatePathPatternToCopy(ExifTool().get_metadata(fileMetadataActive))
+            pathToCopy = self.generatePathPatternToCopy(
+                ExifTool().get_metadata(fileMetadataActive))
             newPathFile = goToDir + os.path.sep + pathToCopy + os.path.sep + fileActive
             if os.path.isfile(newPathFile):
                 index = int(fileActive.rfind(".")) + 1
                 fileType = fileActive[index:]
                 fileName = fileActive[:int(index - 1)]
-                newPathFile = goToDir + os.path.sep + pathToCopy + os.path.sep + fileName + "_COPY_"
-                newPathFile = newPathFile + str(hex(int(round(time.time() * 1000)))).upper() + "." + fileType
+                newPathFile = goToDir + os.path.sep + \
+                    pathToCopy + os.path.sep + fileName + "_COPY_"
+                newPathFile = newPathFile + \
+                    str(hex(int(round(time.time() * 1000)))
+                        ).upper() + "." + fileType
             if self.checkDir(pathToCopy, goToDir):
                 os.rename(fileMetadataActive, newPathFile)
-    
-    #Get GPS Location and create a Path
+
+    # Get GPS Location and create a Path
     def getGPSLocationPath(self, GPSPosition):
         locationRet = ""
         if not GPSPosition in self.geoDic.keys():
-            fullLocationDict = dict(self.geolocator.reverse(GPSPosition, exactly_one=True).raw)
+            fullLocationDict = dict(self.geolocator.reverse(
+                GPSPosition, exactly_one=True).raw)
             locationDict = dict(fullLocationDict['address'])
-            locationRet = locationDict['county'] + '_' + locationDict['state'] +'_' + locationDict['country_code']
-            self.geoDic[GPSPosition] = locationRet 
+            locationRet = locationDict['county'] + '_' + \
+                locationDict['state'] + '_' + locationDict['country_code']
+            self.geoDic[GPSPosition] = locationRet
             self.geoDicNew[GPSPosition] = locationRet
         else:
             locationRet = str(self.geoDic[GPSPosition]).strip()
         return locationRet
-    
-    #Move and organize all files
+
+    # Move and organize all files
     def moveAndOrganizeEachFile(self, pathParam):
         filesAndDir = self.getListFilesAndDir(pathParam)
         goToDir = str(Path.home()) + os.path.sep + "Organized"
@@ -189,19 +203,21 @@ class FileOrganizatorServer:
         for activeDir in filesAndDir[1]:
             newPathParam = pathParam + os.path.sep + activeDir
             self.moveAndOrganizeEachFile(newPathParam)
-    
-    #Save GPS dictionary
+
+    # Save GPS dictionary
     def saveGeoGPSDic(self):
-        with open(self.geoGPSFile,"a") as saveFileGPS:
+        with open(self.geoGPSFile, "a") as saveFileGPS:
             for key, value in self.geoDicNew.items():
                 saveFileGPS.write('%s:%s\n' % (key, value))
         saveFileGPS.close()
-    
-    #start entry point
+
+    # start entry point
     def startFileOrganizator(self):
         self.loadGeoGPSDic()
-        self.md5CheckFile(self.getListFilesAndDir(self.getHomeBridgePath()),self.LOCAL_STR)
+        self.md5CheckFile(self.getListFilesAndDir(
+            self.getHomeBridgePath()), self.LOCAL_STR)
         self.compareMD5()
-        self.sendAFile(self.getHomeBridgePath() + os.path.sep + self.succesFile)
+        self.sendAFile(self.getHomeBridgePath() +
+                       os.path.sep + self.succesFile)
         self.moveAndOrganizeEachFile(self.getHomeBridgePath())
         self.saveGeoGPSDic()
