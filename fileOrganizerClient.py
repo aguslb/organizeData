a2a5a5e226a7248fc5515b6ab91e8e59ba594db3
Agustin Velasco Torres <aguslb@icloud.com>
2020-09-16 21:04:40 -0500

    cambios en comentarios a español en cliente

--- fileOrganizerClient.py
+++ fileOrganizerClient.py
@@ -1,7 +1,8 @@
 import os
-import tarfile
 import pysftp
 import hashlib
+import tarfile
+import datetime
 from pathlib import Path
 
 
@@ -13,6 +14,7 @@ class FileOrganizerClient:
     remoteHomeWin = "H:\\Bridge"
     succesFile = "MD5Result.check"
     privateKey = "/Users/agus/.ssh/id_rsa.fileOrg"
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     # envia los archivos con SFTP remotamente
     def sendFiles(self, filesParam):
@@ -23,7 +25,7 @@ class FileOrganizerClient:
                     for activeFile in filesParam:
                         fileTosend = homepath + os.path.sep + activeFile
                         sftp.put(fileTosend, callback=lambda x, y: print(
-                            "faltan {} de {}".format(((y-x)/1024),y)))
+                            "faltan {} de {}".format(((y-x)/2048),(y/2048))))
         finally:
             sftp.close()
 
@@ -47,7 +49,7 @@ class FileOrganizerClient:
             break
         return files, directory, dirP
 
-    # Check file on client
+    # revisar los archivos con md5
     def md5CheckFile(self, listFilesParam, extend):
         home = self.getHomeBridgePath()
         homeMD5File = str(home) + str(os.path.sep) + \
@@ -64,31 +66,39 @@ class FileOrganizerClient:
         finally:
             fp.close()
 
-    # Entry point
+    # Punto de entrada
     def clientExecute(self):
+        print(self.now + " Start")
         homepath = self.getHomeBridgePath()
         resultFilePath = homepath + os.path.sep + self.succesFile
         if os.path.exists(resultFilePath):
+            print(self.now + " If file exist")
             try:
                 with open(resultFilePath) as fp:
                     for cnt, line in enumerate(fp):
                         print("Line {}: {}".format(cnt, line))
                         rmLineFilePath = homepath + os.path.sep + line
                         if os.path.exists(rmLineFilePath):
+                            print(self.now + " Removing files that where organized successfully")
                             os.remove(rmLineFilePath)
             finally:
                 fp.close()
                 os.remove(resultFilePath)
         listFileDir = self.getListFilesAndDir(homepath)
         for activeDir in listFileDir[1]:
+            print(self.now + " Compressing directory")
             pathTGZ = homepath + os.path.sep + activeDir
             with tarfile.open(pathTGZ + ".tgz", "w:gz") as tar:
                 for name in os.listdir(pathTGZ):
                     tar.add(pathTGZ + os.path.sep + name)
             listFileDir[0].append(str(activeDir + ".tgz"))
+        print(self.now + " Check MD5")
         self.md5CheckFile(listFileDir[0], "")
+        print(self.now + " Add all files")
         listFileDir[0].append(self.MD5File)
+        print(self.now + " Sending files")
         self.sendFiles(listFileDir[0])
+        print(self.now + " End")
 
 
 fOC = FileOrganizerClient()
